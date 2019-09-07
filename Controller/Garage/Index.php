6d88de37dd7ef3608b829d54b4624081092c13bf
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-07 22:10:34 +0600

    no message

--- Controller/Garage/Index.php
+++ Controller/Garage/Index.php
@@ -1,72 +1,38 @@
 <?php
-
 namespace Wolf\Filter\Controller\Garage;
-
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\CacheInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Psr\Log\LoggerInterface;
+class Index extends Action {
+	/**
+	 * Change constructor.
+	 * @param Context $context
+	 * @param LoggerInterface $logger
+	 * @param CacheInterface $cache
+	 * @param StoreManagerInterface $storeManager
+	 * @param CategoryFactory $categoryFactory
+	 * @param CategoryRepositoryInterface $categoryRepository
+	 * @param JsonFactory $resultJsonFactory
+	 */
+	function __construct(
+		Context $context,
+		\Magento\Framework\Registry $registry,
+		LoggerInterface $logger,
+		CacheInterface $cache,
+		JsonFactory $resultJsonFactory
+	) {
+		$this->_registry = $registry;
+		$this->_logger = $logger;
+		$this->_cache = $cache;
+		$this->_resultJsonFactory = $resultJsonFactory;
+		parent::__construct($context);
+	}
 
-class Index extends Action
-{
-
-    /**
-     * Change constructor.
-     * @param Context $context
-     * @param LoggerInterface $logger
-     * @param CacheInterface $cache
-     * @param StoreManagerInterface $storeManager
-     * @param CategoryFactory $categoryFactory
-     * @param CategoryRepositoryInterface $categoryRepository
-     * @param JsonFactory $resultJsonFactory
-     */
-    function __construct(
-        Context $context,
-        \Magento\Framework\Registry $registry,
-        LoggerInterface $logger,
-        CacheInterface $cache,
-        JsonFactory $resultJsonFactory
-    )
-    {
-        $this->_registry = $registry;
-        $this->_logger = $logger;
-        $this->_cache = $cache;
-        $this->_resultJsonFactory = $resultJsonFactory;
-
-        parent::__construct($context);
-    }
-
-    /**
-     *
-     */
-    function execute()
-    {
-
-        $garage = $this->_registry->registry('wolfCategoryCustomerGarage');
-        
-		/* $finalCar = array('cars' => array());
-		
-		foreach($garage['cars'] as $car){
-			
-			if(strpos($car, 'audi') || strpos($car,'volkswagen') || strpos($car,'bmw')){
-			
-              $finalCar['cars'][] = $car;			
-				
-			}
-			
-			
-		} */
-		
-		/* $this->_logger->debug('====');
-		
-		$this->_logger->info(print_r($garage, true));
-		
-		$this->_logger->debug('===='); */
-		
-        $result = $this->_resultJsonFactory->create();
-
-        return $result->setData($garage);
-    }
-
+	function execute() {
+		$garage = $this->_registry->registry('wolfCategoryCustomerGarage');
+		$result = $this->_resultJsonFactory->create();
+		return $result->setData($garage);
+	}
 }
--- Controller/Garage/Remove.php
+++ Controller/Garage/Remove.php
@@ -1,121 +1,101 @@
 <?php
-
 namespace Wolf\Filter\Controller\Garage;
-
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\CacheInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Psr\Log\LoggerInterface;
+class Remove extends Action {
+	/**
+	 * Change constructor.
+	 * @param Context $context
+	 * @param Registry $registry
+	 * @param CustomerFactory $customerResourceFactory
+	 * $param Customer $customerModel
+	 * @param Session $customerSession
+	 * @param LoggerInterface $logger
+	 * @param CacheInterface $cache
+	 * @param JsonFactory $resultJsonFactory
+	 */
+	function __construct(
+		Context $context,
+		\Magento\Framework\Registry $registry,
+		\Magento\Customer\Model\ResourceModel\CustomerFactory $customerResourceFactory,
+		\Magento\Customer\Model\Customer $customerModel,
+		\Magento\Customer\Model\Session $customerSession,
+		LoggerInterface $logger,
+		CacheInterface $cache,
+		JsonFactory $resultJsonFactory
+	)
+	{
+		$this->_registry = $registry;
+		$this->_customerResourceFactory = $customerResourceFactory;
+		$this->_customerModel = $customerModel;
+		$this->_customerSession = $customerSession;
+		$this->_logger = $logger;
+		$this->_cache = $cache;
+		$this->_resultJsonFactory = $resultJsonFactory;
+		parent::__construct($context);
+	}
 
-class Remove extends Action
-{
+	/**
+	 *
+	 */
+	function execute() {
 
-    /**
-     * Change constructor.
-     * @param Context $context
-     * @param Registry $registry
-     * @param CustomerFactory $customerResourceFactory
-     * $param Customer $customerModel
-     * @param Session $customerSession
-     * @param LoggerInterface $logger
-     * @param CacheInterface $cache
-     * @param JsonFactory $resultJsonFactory
-     */
-    function __construct(
-        Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Customer\Model\ResourceModel\CustomerFactory $customerResourceFactory,
-        \Magento\Customer\Model\Customer $customerModel,
-        \Magento\Customer\Model\Session $customerSession,
-        LoggerInterface $logger,
-        CacheInterface $cache,
-        JsonFactory $resultJsonFactory
-    )
-    {
-        $this->_registry = $registry;
-        $this->_customerResourceFactory = $customerResourceFactory;
-        $this->_customerModel = $customerModel;
-        $this->_customerSession = $customerSession;
-        $this->_logger = $logger;
-        $this->_cache = $cache;
-        $this->_resultJsonFactory = $resultJsonFactory;
+		$params = $this->getRequest()->getParams();
+		$errors = array();
+		$customer_garage = array('cars' => array());
 
-        parent::__construct($context);
-    }
+		if(isset($params['uri']) && !empty($params['uri'])) {
 
-    /**
-     *
-     */
-    function execute()
-    {
+			// @todo cleanup uri what happens to in_array if it comes with unknown or huge data?
 
-        $params = $this->getRequest()->getParams();
-        $errors = array();
-        $customer_garage = array('cars' => array());
+		} else {
+			array_push($errors, 'uri must be defined');
+		}
 
-        if(isset($params['uri']) && !empty($params['uri'])) {
+		if(empty($errors)) {
 
-            // @todo cleanup uri what happens to in_array if it comes with unknown or huge data?
+			$customer_garage = $this->_registry->registry('wolfCategoryCustomerGarage');
 
-        } else {
-            array_push($errors, 'uri must be defined');
-        }
+			if(in_array($params['uri'], $customer_garage['cars'])) {
 
-        if(empty($errors)) {
+				// remove the entry from $customer_garage and save
+				$customer_garage['cars'] = array_diff($customer_garage['cars'], [$params['uri']]);
+				$customer_garage_json = json_encode($customer_garage);
+				$customer_id = $this->_customerSession->getCustomer()->getId();
+				if($customer_id) {
 
-            $customer_garage = $this->_registry->registry('wolfCategoryCustomerGarage');
+					$customer = $this->_customerModel->load($customer_id);
+					$customerData = $customer->getDataModel();
+					$customerData->setCustomAttribute('customer_garage_json', $customer_garage_json);
+					$customer->updateData($customerData);
+					$customerResource = $this->_customerResourceFactory->create();
+					$customerResource->saveAttribute($customer, 'customer_garage_json');
+				}
 
-            if(in_array($params['uri'], $customer_garage['cars'])) {
+				$this->_customerSession->setCustomerGarageJson($customer_garage_json);
 
-                // remove the entry from $customer_garage and save
-                $customer_garage['cars'] = array_diff($customer_garage['cars'], [$params['uri']]);
-                $customer_garage_json = json_encode($customer_garage);
+				$this->_registry->unregister('wolfCategoryCustomerGarage');
+				$this->_registry->register('wolfCategoryCustomerGarage', $customer_garage);
 
-                $customer_id = $this->_customerSession->getCustomer()->getId();
-                if($customer_id) {
+			} else {
 
-                    $customer = $this->_customerModel->load($customer_id);
-                    $customerData = $customer->getDataModel();
-                    $customerData->setCustomAttribute('customer_garage_json', $customer_garage_json);
-                    $customer->updateData($customerData);
-                    $customerResource = $this->_customerResourceFactory->create();
-                    $customerResource->saveAttribute($customer, 'customer_garage_json');
+				array_push($errors, 'uri not in garage');
+			}
 
-
-
-//                $customer = $this->_customerSession->getCustomer();
-
-//                    $customer->setCustomAttribute('customer_garage_json', $customer_garage_json);
-//                    $customer->save();
-//                        $customer->setData('customer_garage_json', $customer_garage_json)
-//                            ->setAttributeSetId(\Magento\Customer\Api\CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER)
-//                            ->getResource()->save($customer);
-
-                }
-
-                $this->_customerSession->setCustomerGarageJson($customer_garage_json);
-
-                $this->_registry->unregister('wolfCategoryCustomerGarage');
-                $this->_registry->register('wolfCategoryCustomerGarage', $customer_garage);
-
-            } else {
-
-                array_push($errors, 'uri not in garage');
-            }
-
-        }
+		}
 
 
-        $data = array( 'success' => true, 'params' => $params, 'customer_garage' => $customer_garage);
-        if(!empty($errors)) {
-            $data['errors'] = $errors;
-            $data['success'] = false;
-        }
+		$data = array( 'success' => true, 'params' => $params, 'customer_garage' => $customer_garage);
+		if(!empty($errors)) {
+			$data['errors'] = $errors;
+			$data['success'] = false;
+		}
 
-        $result = $this->_resultJsonFactory->create();
+		$result = $this->_resultJsonFactory->create();
 
-        return $result->setData($data);
-    }
-
+		return $result->setData($data);
+	}
 }
