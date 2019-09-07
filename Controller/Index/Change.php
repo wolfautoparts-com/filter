7d298bb78f6b31703489968150b8612869ca4765
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-07 10:56:30 +0600

    no message

--- Controller/Index/Change.php
+++ Controller/Index/Change.php
@@ -1,41 +1,10 @@
 <?php
 namespace Wolf\Filter\Controller\Index;
 use Df\Framework\W\Result\Json;
-use Magento\Catalog\Api\CategoryRepositoryInterface;
-use Magento\Catalog\Model\CategoryFactory;
 use Magento\Framework\App\Action\Action as _P;
-use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\CacheInterface;
-use Magento\Store\Model\StoreManagerInterface;
-use Psr\Log\LoggerInterface;
 use Wolf\Filter\Observer\Navigation as Ob;
 // 2019-09-06
 class Change extends _P {
-	/**      
-	 * 2019-09-06
-	 * @param Context $context
-	 * @param LoggerInterface $logger
-	 * @param CacheInterface $cache
-	 * @param StoreManagerInterface $storeManager
-	 * @param CategoryFactory $categoryFactory
-	 * @param CategoryRepositoryInterface $categoryRepository
-     */
-	function __construct(
-		Context $context,
-		LoggerInterface $logger,
-		CacheInterface $cache,
-		StoreManagerInterface $storeManager,
-		CategoryFactory $categoryFactory,
-		CategoryRepositoryInterface $categoryRepository
-	) {
-    	$this->_logger = $logger;
-	    $this->_cache = $cache;
-		$this->_storeManager = $storeManager;
-		$this->_categoryInstance = $categoryFactory->create();
-		$this->_categoryRepository = $categoryRepository;
-        parent::__construct($context);    
-    }
-
     /**
 	 * 2019-09-06 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
 	 * «Decrease TTFB (time to first byte) for uncached category pages to 5 seconds»:
