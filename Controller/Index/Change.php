e08300d7976a4a3d4f07394d415d9857d1b69384
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-07 11:33:06 +0600

    no message

--- Controller/Index/Change.php
+++ Controller/Index/Change.php
@@ -16,45 +16,30 @@ class Change extends _P {
 	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
 	 * @return Json
 	 */
-	function execute() {
-		$catId = (int)df_request('selectedValue'); /** @var int $catId «3613» */
-		if (false !== ($data = df_cache_load($cacheId = "category_filter_$catId"))) { /** @var string $cacheId */
-			$categoryArray = unserialize($data);
-		} 
-		else {
-			$levels = (int)df_request('levels'); /** @var int $levels «5» */
-			$dataId = (int)df_request('dataId');/** @var int $dataId «1» */
-			$menuTree = false === ($data = df_cache_load(Ob::CACHE_KEY)) ? [] : unserialize($data);
-			$levelValues = [];
-			$bTree = $menuTree;
-			for ($i = 0; $i < $levels; $i++) {
-				$levelValues[$i] = (int)df_request("level_{$i}_value");
-				if (0 < $levelValues[$i]) {
-					if (isset($bTree[$levelValues[$i]])) {
-						if (isset($bTree[$levelValues[$i]]['children'])) {
-							$bTree = $bTree[$levelValues[$i]]['children'];
-						}
+	function execute() {return Json::i(df_cache_get_simple('category_filter_' . df_request('selectedValue'), function() {
+		$levels = (int)df_request('levels'); /** @var int $levels «5» */
+		$dataId = (int)df_request('dataId');/** @var int $dataId «1» */
+		$menuTree = false === ($data = df_cache_load(Ob::CACHE_KEY)) ? [] : unserialize($data);
+		$levelValues = [];
+		$bTree = $menuTree;
+		for ($i = 0; $i < $levels; $i++) {
+			$levelValues[$i] = (int)df_request("level_{$i}_value");
+			if (0 < $levelValues[$i]) {
+				if (isset($bTree[$levelValues[$i]])) {
+					if (isset($bTree[$levelValues[$i]]['children'])) {
+						$bTree = $bTree[$levelValues[$i]]['children'];
 					}
 				}
 			}
-			$categoryArray = [];
-			foreach ($bTree as $menuEntry) {
-				array_push($categoryArray, [
-					'id' => $menuEntry['id'], 'name' => $menuEntry['name'], 'url' => $menuEntry['url']
-				]);
-			}
-			if ($dataId == 1) {// For changing sort(desc) order of Year
-				usort($categoryArray, function ($first, $second) {return
-					strtolower($first['name']) < strtolower($second['name'])
-				;});
-			} 
-			else { // For rest dropdown
-				usort($categoryArray, function ($first, $second) {return
-					strtolower($first['name']) > strtolower($second['name'])
-				;});
-			}
-			df_cache_save(serialize($categoryArray), $cacheId, [Ob::CACHE_TAG]);
 		}
-		return Json::i($categoryArray);
-	}
+		$r = [];
+		foreach ($bTree as $menuEntry) {
+			array_push($r, [
+				'id' => $menuEntry['id'], 'name' => $menuEntry['name'], 'url' => $menuEntry['url']
+			]);
+		}
+		$r = df_sort($r, function($a, $b) {return strcasecmp($a['name'], $b['name']);});
+		// 2019-09-07 We show years in the reverse order.
+		return 1 !== $dataId ? $r : array_reverse($r);
+	}, [Ob::CACHE_TAG]));}
 }
