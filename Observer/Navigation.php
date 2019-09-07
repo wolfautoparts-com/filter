bad11efd64f40c68e8bc98bfd6523c1d8d8d27c5
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-07 17:07:30 +0600

    no message

--- Observer/Navigation.php
+++ Observer/Navigation.php
@@ -1,69 +1,25 @@
 <?php
-
 namespace Wolf\Filter\Observer;
-
+use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use \Magento\Framework\Event\Observer;
-use Magento\Framework\App\CacheInterface;
-use Magento\Framework\Registry;
-use Magento\Customer\Setup\CustomerSetupFactory;
-use \Psr\Log\LoggerInterface;
-
-
-class Navigation implements ObserverInterface
-{
-
-	private $_logger;
-	private $_cache;
-    private $_registry;
-
-
-	function __construct(
-		LoggerInterface $logger,
-		CacheInterface $cache,
-        Registry $registry,
-        \Magento\Customer\Model\Session $customerSession,
-        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
-	)
-	{
-		$this->_logger = $logger;
-		$this->_cache = $cache;
-		$this->_registry = $registry;
-        $this->_customerSession = $customerSession;
-        $this->_customerRepository = $customerRepository;
-
-	}
-
+class Navigation implements ObserverInterface {
 	function execute(Observer $observer) {
-		$cacheTags = [self::CACHE_TAG];
-		if (false !== ($data = $this->_cache->load(self::CACHE_KEY))) {
-			$menuTree = unserialize($data);
+		if (!($menuTree = wolf_tree_load())) {
+			$menuTree = $this->_getMenuTree($observer->getData('menu'));
+			df_cache_save(serialize($menuTree), self::CACHE_KEY, [self::CACHE_TAG]);
 		}
-		else {
-			$menu = $observer->getData('menu');
-			$this->_logger->debug('$menu');
-			$this->_logger->debug(json_encode($menu));
-			$menuTree = $this->_getMenuTree($menu);
-			$this->_cache->save(serialize($menuTree), self::CACHE_KEY, $cacheTags);
-		}
-		// Store the menuTree on registry for later use on Block Navigation
-        $this->_registry->register('wolfCategoryMenuTree', $menuTree);
+		df_register('wolfCategoryMenuTree', $menuTree);
 	}
 
 	// @todo code compress difference this
 	protected function _getMenuTree ($menuTree) {
-
-
 		$children = $menuTree->getChildren();
 		$parentLevel = $menuTree->getLevel();
 		$childLevel = $parentLevel === null ? 0 : $parentLevel + 1;
-
 		$counter = 1;
 		$itemPosition = 0;
 		$childrenCount = $children->count();
-
 		$result = array();
-
 		foreach ($children as $child) {
 			if ( $childLevel === 0 && $child->getData( 'is_parent_active' ) === false ) {
 				continue;
@@ -75,11 +31,8 @@ class Navigation implements ObserverInterface
 			$id = $child->getId();
 			$id = str_replace('category-node-', '', $id);
 			$id = (int) $id;
-
 			if($childrenCount) {
-
 				$subChildren = $this->_getMenuTree($child);
-
 				if($subChildren && !empty($subChildren)) {
 					$result[$id] = array(
 						'id' => $id,
@@ -94,7 +47,6 @@ class Navigation implements ObserverInterface
 						'url' => $child->getUrl()
 					);
 				}
-
 			} else {
 				$result[$id] = array(
 					'id' => $id,
@@ -102,15 +54,10 @@ class Navigation implements ObserverInterface
 					'url' => $child->getUrl()
 				);
 			}
-
-
 			$itemPosition++;
 			$counter++;
 		}
-
-
 		return $result;
-
 	}
 
 	/**
--- lib/main.php
+++ lib/main.php
@@ -5,6 +5,7 @@ use Wolf\Filter\Observer\Navigation as Ob;
  * @used-by \Wolf\Filter\Block\Navigation::getConfigJson()
  * @used-by \Wolf\Filter\Controller\Garage\AllCars::execute()
  * @used-by \Wolf\Filter\Controller\Index\Change::execute()
+ * @used-by \Wolf\Filter\Observer\Navigation::execute()
  * @return mixed[]
  */
 function wolf_tree_load() {return false === ($r = df_cache_load(Ob::CACHE_KEY)) ? [] : unserialize($r);}
