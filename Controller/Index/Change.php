018907a928714dc72a40643adac9a43e8b368034
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-07 10:53:44 +0600

    no message

--- Controller/Index/Change.php
+++ Controller/Index/Change.php
@@ -1,16 +1,16 @@
 <?php
 namespace Wolf\Filter\Controller\Index;
+use Df\Framework\W\Result\Json;
 use Magento\Catalog\Api\CategoryRepositoryInterface;
 use Magento\Catalog\Model\CategoryFactory;
-use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Action as _P;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\CacheInterface;
-use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Store\Model\StoreManagerInterface;
 use Psr\Log\LoggerInterface;
 use Wolf\Filter\Observer\Navigation as Ob;
 // 2019-09-06
-class Change extends Action {
+class Change extends _P {
 	/**      
 	 * 2019-09-06
 	 * @param Context $context
@@ -19,7 +19,6 @@ class Change extends Action {
 	 * @param StoreManagerInterface $storeManager
 	 * @param CategoryFactory $categoryFactory
 	 * @param CategoryRepositoryInterface $categoryRepository
-	 * @param JsonFactory $resultJsonFactory
      */
 	function __construct(
 		Context $context,
@@ -27,15 +26,13 @@ class Change extends Action {
 		CacheInterface $cache,
 		StoreManagerInterface $storeManager,
 		CategoryFactory $categoryFactory,
-		CategoryRepositoryInterface $categoryRepository,
-		JsonFactory $resultJsonFactory
+		CategoryRepositoryInterface $categoryRepository
 	) {
     	$this->_logger = $logger;
 	    $this->_cache = $cache;
 		$this->_storeManager = $storeManager;
 		$this->_categoryInstance = $categoryFactory->create();
 		$this->_categoryRepository = $categoryRepository;
-		$this->_resultJsonFactory = $resultJsonFactory;
         parent::__construct($context);    
     }
 
@@ -43,6 +40,12 @@ class Change extends Action {
 	 * 2019-09-06 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
 	 * «Decrease TTFB (time to first byte) for uncached category pages to 5 seconds»:
 	 * https://www.upwork.com/ab/f/contracts/22684975
+	 * @override
+	 * @see _P::execute()
+	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
+	 * 		$result = $this->execute();
+	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
+	 * @return Json
 	 */
 	function execute() {
 		$catId = (int)df_request('selectedValue'); /** @var int $catId «3613» */
@@ -90,7 +93,6 @@ class Change extends Action {
 			}
 			$this->_cache->save(serialize($categoryArray), $cacheId, $cacheTags);
 		}
-		$result = $this->_resultJsonFactory->create();
-		return $result->setData($categoryArray);
+		return Json::i($categoryArray);
 	}
 }
