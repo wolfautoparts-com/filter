ccce75b794b8784f07c68b008f7fa80d4c35af39
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-09 11:49:37 +0600

    no message

--- lib/cache.php
+++ lib/cache.php
@@ -3,11 +3,10 @@ use Wolf\Filter\Observer\TopMenuGetHTMLBefore as Ob;
 
 /**
  * 2019-09-07
+ * 2019-09-09 We can not cache the result.
  * @used-by \Wolf\Filter\Block\Navigation::topLevel()
  * @used-by \Wolf\Filter\Controller\Index\Change::execute()
  * @used-by \Wolf\Filter\Observer\TopMenuGetHTMLBefore::execute()
  * @return mixed[]
  */
-function wolf_tree_load() {return dfcf(function() {return
-	false === ($r = df_cache_load(Ob::CACHE_KEY)) ? [] : unserialize($r)
-;});}
+function wolf_tree_load() {return false === ($r = df_cache_load(Ob::CACHE_KEY)) ? [] : unserialize($r);}
