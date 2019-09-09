ab0946759393177db537d8db3f3f49471e575fff
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-09 07:50:40 +0600

    no message

--- Controller/Garage/Index.php
+++ Controller/Garage/Index.php
@@ -3,10 +3,11 @@ namespace Wolf\Filter\Controller\Garage;
 use Df\Framework\W\Result\Json;
 use Magento\Framework\App\Action\Action as _P;
 use Wolf\Filter\Customer as WCustomer;
+// 2019-09-09
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Index extends _P {
     /**
-	 * 2019-09-08
+	 * 2019-09-09
 	 * @override
 	 * @see _P::execute()
 	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
